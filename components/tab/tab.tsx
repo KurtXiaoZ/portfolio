59dd34a5e9ef8455654e385b448fe882bf287025
KurtXiaoZ <kurtxiaoz@gmail.com>
2026-09-09 22:29:43 -0400

    fix: align tab typography with introduction

--- components/tab/tab.tsx
+++ components/tab/tab.tsx
@@ -15,7 +15,7 @@ export function Tab({
     <button
       aria-selected={active}
       className={clsx(
-        'cursor-pointer border-0 bg-transparent p-0 text-inherit decoration-1 underline-offset-4 focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-[#657800] disabled:cursor-default disabled:opacity-50 dark:focus-visible:outline-[#c6ec39]',
+        'cursor-pointer border-0 bg-transparent p-0 text-lg leading-7 text-[#62635d] decoration-1 underline-offset-4 focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-[#657800] disabled:cursor-default disabled:opacity-50 dark:text-[#a8aaa2] dark:focus-visible:outline-[#c6ec39]',
         active && 'underline',
         className,
       )}
